c2fd2803ecf203265186cdfd847575cc5951313b
Jamie-Page <jamie_page1998@hotmail.com>
2021-06-13 12:37:34 +0000

    added flips cards & parts of the grid

--- ms2-project/assets/js/script.js
+++ ms2-project/assets/js/script.js
@@ -26,15 +26,47 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const grid = document.querySelector('.grid')
+var cardsChosen = []
+var cardsChosenId = []
+
+
 /* creating the board */ 
 
-functuion createBoard() {
+function createBoard() {
 
-    for (let i = 0 < cardArray, length; i++) {
+    for (let i = 0; i < cardArray, length; i++) {
         var card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png') /* need to change this for my images in future */
         card.setAttribute('data-id', i)
         // card.addEventListener('click', flipcard)
+        grid.appendChild(card) //grid here and the grid uptop match
+
+
     }
 
 }
+//check for matches 
+function checkForMatch() {
+    var cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
+    if (cardsChosen[0] === cardsChosen [1]) {
+        alert('You founf a match')
+        cards[optionOneId].setAttribute('src', 'images ')
+    }
+}
+
+
+//flip your card
+
+function flipCard(){
+  var cardId = this.getAttribute('data-id')  
+  cardsChosen.push(cardsArray[cardId].name)
+  cardsChosenId.push(cardId)
+  this.setAttribute('src',cardArray[cardId].img)
+  if (cardsChosen.length === 2 ) {
+      setTimeout(checkForMatch, 500)
+  }
+}
+
+createBoard()
