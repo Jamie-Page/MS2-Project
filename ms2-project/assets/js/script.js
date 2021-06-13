8fff9c63704f7c3f1711db11ba613526c178921d
Jamie-Page <jamie_page1998@hotmail.com>
2021-06-13 11:54:53 +0000

    added images section to the scrip

---
+++ ms2-project/assets/js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options 
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+         {
+            name: 'fries', /* need to change all of these for each image chosen */
+            img: 'images/fries.png'
+        },
+         {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+         {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+         {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+    ]
+
+})
+
+/* creating the board */ 
