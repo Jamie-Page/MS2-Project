2aa1a7d5d8ed764723f64972791ac76ebece8d65
Jamie-Page <jamie_page1998@hotmail.com>
2021-06-15 18:39:42 +0000

    trying to get the board to work

--- memory-game/assets/js/script.js
+++ memory-game/assets/js/script.js
@@ -3,51 +3,51 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardArray = [  /* Two of each for a matching set of cards */
         {
             name: 'ada',
-            img: 'images/ada.jpg'
+            img: 'memory-game/images/ada.jpg'
         },
          {
             name: 'ada', 
-            img: 'images/ada.jgp'
+            img: 'memory-game/images/ada.jpg'
         },
          {
             name: 'bitcoin',
-            img: 'images/bitcoin.jpg'
+            img: 'memory-game/images/bitcoin.jpg'
         },
          {
             name: 'bitcoin',
-            img: 'images/bitcoin.jpg'
+            img: 'memory-game/images/bitcoin.jpg'
         },
          {
             name: 'etherium',
-            img: 'images/etherium.jpg'
+            img: 'memory-game/images/etherium.jpg'
         },
         {
             name: 'etherium',
-            img: 'images/etherium.jpg'
+            img: 'memory-game/images/etherium.jpg'
         },
         {
             name: 'litecoin',
-            img: 'images/litecoin.jpg'
+            img: 'memory-game/images/litecoin.jpg'
         },
         {
             name: 'litecoin',
-            img: 'images/litecoin.jpg'
+            img: 'memory-game/images/litecoin.jpg'
         },
         {
             name: 'neo',
-            img: 'images/neo.jpg'
+            img: 'memory-game/images/neo.jpg'
         },
         {
             name: 'neo',
-            img: 'images/neo.jpg'
+            img: 'memory-game/images/neo.jpg'
         },
          {
             name: 'tether',
-            img: 'images/tether.jpg'
+            img: 'memory-game/images/tether.jpg'
         },
         {
             name: 'tether',
-            img: 'images/tether.jpg'
+            img: 'memory-game/images/tether.jpg'
         },
     ]
 
@@ -64,54 +64,58 @@ var cardsWon = []/* PUT A VAR EACH TIME A NEW POSSIBLITY COMES IN */
 
 /* creating the board */ 
 
-function createBoard() {
+cardArray.sort(()=>0.5-Math.random())
+    const grid = document.querySelector('.grid');
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = [];
+    var cardsChosenId = [];
+    var cardsWon = [];/* PUT A VAR EACH TIME A NEW POSSIBLITY COMES IN */
+    //create your board
 
-    for (let i = 0; i < cardArray, length; i++) {
-        var card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.jpg') /* need to change this for my images in future */
-        card.setAttribute('data-id', i)
-        // card.addEventListener('click', flipcard)
-        grid.appendChild(card) //grid here and the grid uptop match
 
+    function createBoard(){
+        for(let i = 0;i<cardArray.length;i++){
+          var card = document.createElement('img')
+          card.setAttribute('src','memory-game/images/blank.jpg')
+          card.setAttribute('data-id',i)
+          card.addEventListener('click',flipCard)
+          grid.appendChild(card)
+        }
+    }
+    //check matches
+    function checkForMatch(){
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if(cardsChosen[0]===cardsChosen[1]){
+            alert('You Found A Match!!!')
+            cards[optionOneId].setAttribute('src','memory-game/images/white.jpg')
+            cards[optionTwoId].setAttribute('src','memory-game/images/white.jpg')
+            cardsWon.push(cardsChosen)
+        }
+        else {
+            cards[optionOneId].setAttribute('src','memory-game/images/blank.jgp')
+            cards[optionTwoId].setAttribute('src','memory-game/images/blank.jgp')
+            alert('Sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if(cardsWon.length===cardArray.length/2){
+            resultDisplay.textContent ='Congratulations! you found them all!'
 
+        }
     }
 
-}
-//check for matches 
-function checkForMatch() {
-    var cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]
-    if (cardsChosen[0] === cardsChosen [1]) {
-        alert('You found a match.')
-        cards[optionOneId].setAttribute('src', 'images/white.jpg')
-        cards[optionTwoId].setAttribute('src', 'images/white.jpg')
-        cardsWon.push(cardsChosen)
-    } else {
-       cards[optionOneId].setAttribute('src', 'images/blank.jpg')
-       cards[optionTwoId].setAttribute('src', 'images/blank.jpg')
-       alert('Sorry, Please try again.')
+    //flip your card
+    function flipCard(){
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src',cardArray[cardId].img)
+        if (cardsChosen.length===2)
+        {
+            setTimeout(checkForMatch,500)
+        }
     }
-    cardsChosen = []
-    cardsChosenId = []
-    resultDisplay.textContent = cardsWon.length
-    if (cardsWon.length === cardsArray.length/2) {
-        resultDisplay.textContent = 'congratulation! You found them all'
-    }
-    
-}
-
-
-//flip your card
-
-function flipCard(){
-  var cardId = this.getAttribute('data-id')  
-  cardsChosen.push(cardsArray[cardId].name)
-  cardsChosenId.push(cardId)
-  this.setAttribute('src',cardArray[cardId].img)
-  if (cardsChosen.length === 2 ) {
-      setTimeout(checkForMatch, 500)
-  }
-}
-
-createBoard()
+    createBoard();
